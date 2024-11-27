268336b5a6579417b069dbaf24de129e2e659824
Pavel <paulkapustin@gmail.com>
2024-11-27 19:09:50 +0300

    Selection sort

--- Task_3.py
+++ Task_3.py
@@ -1,1 +1,9 @@
-print ('Начинаю 3-е задание')
+#Функция "Сортировка выбором"
+def selection_sort(array):
+    for i in range (len(array)-1):
+        the_smallest = i
+        for j in range (i+1, len(array)):
+            if array[j] < array [the_smallest]:
+                the_smallest = j
+        array[i], array[the_smallest] = array[the_smallest], array[i]
+    return array
