d320636636bbc8071b2b09e2a02a99d11955b836
Pavel <paulkapustin@gmail.com>
2024-11-27 19:44:15 +0300

    Massive generation

--- Task_3_2.py
+++ Task_3_2.py
@@ -6,3 +6,11 @@ try:
     n = int(input ("Введите целое положительное число:    "))
 except ValueError:
     print ("Необходимо ввести целое число, нельзя вводить дробные")
+
+#Генерирум массив случайных чисел
+array_1 = []
+for i in range(n):
+    array_1.append(randint(1,1000))
+print (array_1)
+
+print(selection_sort(array_1))
